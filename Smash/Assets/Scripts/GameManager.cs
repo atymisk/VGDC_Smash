524b5f087760db853ef67d0a746ebee607399710
Reddeyfish <edrichd@uci.edu>
2015-02-15 17:57:04 -0800

    Found a way to disable collisions between two objects
    This is supposed to be impossible. What did I do wrong?

--- Smash/Assets/Scripts/GameManager.cs
+++ Smash/Assets/Scripts/GameManager.cs
@@ -6,5 +6,8 @@ public class GameManager : MonoBehaviour {
 	void Awake ()
 	{
 		Application.targetFrameRate = 60;
+        CapsuleCollider one = GameObject.Find("player").GetComponent<CapsuleCollider>();
+        CapsuleCollider two = GameObject.Find("player2").GetComponent<CapsuleCollider>();
+        Physics.IgnoreCollision(one, two, true);
 	}
 }
