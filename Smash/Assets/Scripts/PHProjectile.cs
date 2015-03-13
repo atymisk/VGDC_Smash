e44b8da453101a7b9f780d5bfe901297508e0e60
Reddeyfish <edrichd@uci.edu>
2015-03-12 21:44:44 -0700

    Projectiles now work, but don't do anything on hit

---
+++ Smash/Assets/Scripts/PHProjectile.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PHProjectile : Projectile {
+
+    protected override void SetStartVelocity()
+    {
+        rigidbody.velocity = transform.InverseTransformDirection(new Vector3(10, 0, 0)); // set the velocity forward in our current facing
+    }
+}
---
+++ Smash/Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Projectile : MonoBehaviour {
+
+    //abstract class for all projectiles
+    public int durationRemaining_frames; //how many frames will this projectile stay active
+	// Use this for initialization
+	void Start () {
+        SetStartVelocity();
+	}
+    void FixedUpdate()
+    {
+        durationRemaining_frames -= 1;
+        if (durationRemaining_frames <= 0)
+            Destroy(this.gameObject);
+        OnUpdate();
+    }
+
+    protected void OnUpdate() {} // overriding this is optional
+    protected abstract void SetStartVelocity();
+}
